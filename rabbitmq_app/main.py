58ab36e8b67f048210e2ec83b0ca765080c20fc9
kamlesh-bannagare <kamleshbannagare1@gmail.com>
2024-06-26 12:42:28 +0530

    placed import statement on top

--- rabbitmq_app/main.py
+++ rabbitmq_app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-
+import threading
+import uvicorn
 # Import routers and data processing functions from their respective locations
 from Assignment_rabbitmqt_upswing.rabbitmq_app.routes import router  # Assuming routes are defined here
 from Assignment_rabbitmqt_upswing.rabbitmq_app.process_rabbitmq_data import start_mqtt_consumer  # Assuming data processing happens here
@@ -7,7 +8,7 @@ from Assignment_rabbitmqt_upswing.rabbitmq_app.process_rabbitmq_data import star
 # Create a FastAPI application instance
 app = FastAPI()
 
-# Include the router defined in the `routes.py` file (adjust path if needed)
+# Include the router defined in the `routes.py` file
 app.include_router(router)
 
 # Define an event handler that runs on application startup
@@ -18,13 +19,11 @@ async def startup_event():
     on application startup.
     """
 
-    import threading
     # Create a background thread to run the consumer function
     consumer_thread = threading.Thread(target=start_mqtt_consumer, daemon=True)
     consumer_thread.start()
 
-# Entry point for running the application (assuming uvicorn is used)
+# Entry point for running the application
 if __name__ == "__main__":
-    import uvicorn
     # Configure and run the uvicorn server
     uvicorn.run(app, host="0.0.0.0", port=8002)
